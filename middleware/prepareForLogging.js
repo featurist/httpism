e19479f82d5c99dba8ad14b7de8f0da2945c27e4
Tim Macfarlane <timmacfarlane@gmail.com>
2019-04-11 11:51:05 +0200

    obfuscate Authorization headers when logging

--- middleware/prepareForLogging.js
+++ middleware/prepareForLogging.js
@@ -5,15 +5,27 @@ module.exports = function prepareForLogging (r) {
   return removeUndefined({
     method: r.method,
     url: r.url && obfuscateUrlPassword(r.url),
-    headers: r.headers,
+    headers: obfuscateHeaders(r.headers),
     body: isStream(r.logBody) ? '[Stream]' : r.logBody,
     statusCode: r.statusCode,
     statusText: r.statusText
   })
 }
 
+function obfuscateHeaders (headers) {
+  var result = {}
+  Object.keys(headers).forEach(function (key) {
+    if (key === 'authorization') {
+      result[key] = String(headers[key]).split(' ')[0] + ' ********'
+    } else {
+      result[key] = headers[key]
+    }
+  })
+  return result
+}
+
 function removeUndefined (obj) {
-  Object.keys(obj).map(function (key) {
+  Object.keys(obj).forEach(function (key) {
     if (typeof obj[key] === 'undefined') {
       delete obj[key]
     }
